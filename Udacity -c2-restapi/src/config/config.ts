5b60766c1788ce1106731751781df621251cf7a3
suryansh1168 <46718550+suryansh1168@users.noreply.github.com>
2020-06-12 03:54:08 +0530

    Add files via upload

---
+++ Udacity -c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramudacity",
+    "password": "123456789",
+    "database": "udagramudacity",
+    "host": "udagramudacity.crfyvd0wfi2a.ap-south-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-south-1b",
+    "aws_profile": "default",
+    "aws_media_bucket": "project-udacity-udagram-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagramudacity",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
